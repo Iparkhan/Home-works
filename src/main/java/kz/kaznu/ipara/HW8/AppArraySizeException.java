6071161d250fb2abb65ea26850d9d0272cd31300
Iparkhan <yunnsvii@gmail.com>
2025-03-10 16:10:07 +0600

    HW8

---
+++ src/main/java/kz/kaznu/ipara/HW8/AppArraySizeException.java
@@ -0,0 +1,7 @@
+package kz.kaznu.ipara.HW8;
+
+public class AppArraySizeException extends Exception {
+    public AppArraySizeException(String message) {
+        super(message);
+    }
+}
--- src/main/java/kz/kaznu/ipara/HW8/Cat.java
+++
@@ -1,7 +0,0 @@
-package kz.kaznu.ipara.HW8;
-
-public class Cat {
-    public static void main(String[] args) {
-        System.out.println("viu");
-    }
-}
